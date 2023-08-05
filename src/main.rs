e4ab2a8538b9b3e10877ee04450ad6650d94217f
Anoop Elias <anoopelias@gmail.com>
2023-08-05 16:47:04 +0530

    Validate instructions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use rustyline::error::ReadlineError;
 use rustyline::{DefaultEditor, Result};
+use wast::core::Instruction;
+use wast::parser::{Parse, Parser, ParseBuffer, self};
+
 
 fn main() -> Result<()> {
     let mut rl = DefaultEditor::new()?;
@@ -8,7 +11,7 @@ fn main() -> Result<()> {
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str())?;
-                println!("Line: {}", line);
+                parse_line(&line.as_str());
             },
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
@@ -25,4 +28,31 @@ fn main() -> Result<()> {
         }
     }
     Ok(())
+}
+
+fn parse_line(str: &str) {
+    let buf = ParseBuffer::new(str).unwrap();
+    match parser::parse::<InstructionsParser>(&buf) {
+        Ok(_) => println!("Ok"),
+        Err(e) => {
+            println!("Error: {}", e.message());
+            return
+        }
+    };
+}
+
+struct InstructionsParser<'a> {
+    #[allow(dead_code)]
+    instructions: Vec<Instruction<'a>>,
+}
+
+impl<'a> Parse<'a> for InstructionsParser<'a> {
+    fn parse(parser: Parser<'a>) -> wast::parser::Result<Self> {
+        let mut instructions = Vec::new();
+        while !parser.is_empty() {
+            let instruction = parser.parse()?;
+            instructions.push(instruction);
+        }
+        Ok(InstructionsParser { instructions })
+    }
 }
