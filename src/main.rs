0ed0574f37b7a3d0684ce0d7ebb1b6feac13d23b
Anoop Elias <anoopelias@gmail.com>
2023-08-06 15:38:13 +0530

    Support warpping paranthesis

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rustyline::error::ReadlineError;
 use rustyline::{DefaultEditor, Result};
-use wast::core::Instruction;
-use wast::parser::{Parse, Parser, ParseBuffer, self};
+use wast::core::Expression;
+use wast::parser::{ParseBuffer, self};
 
 
 fn main() -> Result<()> {
@@ -32,27 +32,11 @@ fn main() -> Result<()> {
 
 fn parse_line(str: &str) {
     let buf = ParseBuffer::new(str).unwrap();
-    match parser::parse::<InstructionsParser>(&buf) {
+    match parser::parse::<Expression>(&buf) {
         Ok(_) => println!("Ok"),
         Err(e) => {
             println!("Error: {}", e.message());
             return
         }
     };
-}
-
-struct InstructionsParser<'a> {
-    #[allow(dead_code)]
-    instructions: Vec<Instruction<'a>>,
-}
-
-impl<'a> Parse<'a> for InstructionsParser<'a> {
-    fn parse(parser: Parser<'a>) -> wast::parser::Result<Self> {
-        let mut instructions = Vec::new();
-        while !parser.is_empty() {
-            let instruction = parser.parse()?;
-            instructions.push(instruction);
-        }
-        Ok(InstructionsParser { instructions })
-    }
 }
