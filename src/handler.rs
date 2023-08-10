14e339758aea146503befca34f66090f4a1dc52f
Anoop Elias <anoopelias@gmail.com>
2023-08-10 22:26:45 +0530

    Add `drop` instruction implementation

--- src/handler.rs
+++ src/handler.rs
@@ -19,6 +19,17 @@ impl<'a> Handler<'a> for I32ConstInstr<'a> {
     }
 }
 
+struct DropInstr<'a> {
+    state: &'a mut State,
+}
+
+impl<'a> Handler<'a> for DropInstr<'a> {
+    fn handle(&mut self) -> Result<()> {
+        self.state.stack.pop()?;
+        Ok(())
+    }
+}
+
 pub fn handler_for<'a>(
     instr: &Instruction,
     state: &'a mut State,
@@ -28,6 +39,7 @@ pub fn handler_for<'a>(
             value: *value,
             state,
         })),
+        Instruction::Drop => Ok(Box::new(DropInstr { state })),
         _ => Err(Error::msg("Unknown instruction")),
     }
 }
@@ -35,17 +47,33 @@ pub fn handler_for<'a>(
 #[cfg(test)]
 mod tests {
     use crate::{executor::State, handler::handler_for};
+    use anyhow::Result;
 
     use wast::core::Instruction;
+
+    fn exec_instr(instr: &Instruction, state: &mut State) -> Result<()> {
+        let mut handler = handler_for(instr, state).unwrap();
+        handler.handle()
+    }
 
     #[test]
     fn test_i32_const() {
         let mut state = State::new();
-        let instr = Instruction::I32Const(42);
-        {
-            let mut handler = handler_for(&instr, &mut state).unwrap();
-            handler.handle().unwrap();
-        }
+        exec_instr(&Instruction::I32Const(42), &mut state).unwrap();
         assert_eq!(state.stack.pop().unwrap(), 42);
+    }
+
+    #[test]
+    fn test_drop() {
+        let mut state = State::new();
+        state.stack.push(42);
+        exec_instr(&Instruction::Drop, &mut state).unwrap();
+        assert!(state.stack.pop().is_err());
+    }
+
+    #[test]
+    fn test_drop_error() {
+        let mut state = State::new();
+        assert!(exec_instr(&Instruction::Drop, &mut state).is_err());
     }
 }
