03fdfb3173274499a0111175df0815f62e00de1e
Anoop Elias <anoopelias@gmail.com>
2023-08-16 23:37:23 +0530

    Remove `dead_code` warning suppression for `locals` functions
    Since these are in use now.

--- src/locals.rs
+++ src/locals.rs
@@ -29,7 +29,6 @@ impl Locals {
         self.values.set(index, value)
     }
 
-    #[allow(dead_code)]
     pub fn set_by_id(&mut self, id: &str, value: i32) -> Result<()> {
         let index = self.ids.get(id)?;
         self.set(index, value)
@@ -39,7 +38,6 @@ impl Locals {
         self.values.get(index)
     }
 
-    #[allow(dead_code)]
     pub fn get_by_id(&self, id: &str) -> Result<i32> {
         let index = self.ids.get(id)?;
         self.get(index)
