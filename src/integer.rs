8e9654b8b262ac03397c33055b0ac2adf155e11f
Anoop Elias <anoopelias@gmail.com>
2023-08-23 13:22:57 +0530

    Add tests for clone / display

--- src/integer.rs
+++ src/integer.rs
@@ -77,6 +77,18 @@ mod tests {
     use crate::integer::Integer;
 
     #[test]
+    fn test_clone() {
+        assert_eq!(Integer::I32(1).clone(), Integer::I32(1));
+        assert_eq!(Integer::I64(1).clone(), Integer::I64(1));
+    }
+
+    #[test]
+    fn test_display() {
+        assert_eq!(format!("{}", Integer::I32(1)), "1");
+        assert_eq!(format!("{}", Integer::I64(4)), "4");
+    }
+
+    #[test]
     fn test_leading_zeros() {
         assert_eq!(Integer::I32(1).leading_zeros(), Integer::I32(31));
         assert_eq!(Integer::I64(1).leading_zeros(), Integer::I64(63));
